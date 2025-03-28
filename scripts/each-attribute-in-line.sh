73455e695137444302bb2cc6d835f4d4de8be8a4
Marc Wimmer <marc@zebroo.de>
2025-03-28 20:41:23 +0100

    .

--- scripts/each-attribute-in-line.sh
+++ scripts/each-attribute-in-line.sh
@@ -5,7 +5,7 @@ SOURCE_DIR=$(dirname "$(readlink -f "$0")")
 XSLT="$SOURCE_DIR/each-attribute-in-line.xslt"
 
 for file in "$@"; do
-  echo "$XSLT"
+  cat "$XSLT"
   tmp=$(mktemp)
   xsltproc $XSLT "$file" | xmlstarlet fo > "$tmp"
   mv "$tmp" "$file"
